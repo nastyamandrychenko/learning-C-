8e48aef23223699ff72e6615d99c68e4f3170273
Nastya <nastyamarichenko@gmail.com>
2022-08-15 21:54:43 +0200

    Created project "Variables" with multiplication

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("dfsf");
+            Console.ReadKey();
         }
     }
 }
---
+++ Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, num1;
+
+            Console.WriteLine("Multiplication");
+            Console.WriteLine("Enter first value");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second value");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The resut is " + (num * num1));
+            Console.ReadKey();
+        }
+    }
+}
