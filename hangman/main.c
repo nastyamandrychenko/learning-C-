9bec8d29256043c7cc9bd2199755223aa751186a
Nastya <nastyamarichenko@gmail.com>
2022-11-13 14:03:57 +0200

    random word from array

--- hangman/main.c
+++ hangman/main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <ctype.h>
-#define __STDC_WANT_LIB_EXT2__ 1
 #include <string.h>
 
 int main(void)
@@ -23,20 +22,36 @@ int main(void)
     countWords++;
     }
     printf("%d", countWords);
+   // rewind() function repositions the file pointer associated with stream to the beginning of the file
     rewind(file);
+
     char **array = malloc(countWords * sizeof(char*));
     x = 0;
-    while ( fscanf(file, "%29s", word) == 1) {
+    // fill dynamic array with words
+    while ( fscanf(file, "%s", word) == 1) {
+        // strdup() - duplicate a string
     array[x] = strdup(word);
     x++;
     }
+
    fclose(file);
-   for(int i = 0; i < countWords; i++){
-    printf("%s ", array[i]);
-   }
+
+//    for(int i = 0; i < countWords; i++){
+//     printf("%s ", array[i]);
+//    }
+
+// initialize the seed of random in each run of the code to get a different random result by srand. 
+// using time(NULL) to set a different seed of random through srand
+    char *random; 
+    srand(time(NULL));
+    random = array[rand() % countWords]; 
+    printf("\n%s",random); 
+
+  //teardown
   for (x = 0; x < countWords; x++)
     free(array[x]);
   free(array);
+
     // printf("\n\n\t\tWELCOME TO HANGMAN!!!");
     // printf("\n\n\t\t\tRULES:");
     // printf("\n\t - Maximum 10 mistakes are allowed.");
